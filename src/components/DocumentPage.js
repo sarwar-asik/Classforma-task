80e25bbedf4e8bd863fbbb09c39958c48c0d851e
sarwar-asik <shasik2021@gmail.com>
2022-12-24 16:02:58 +0600

    iframe implement

--- src/components/DocumentPage.js
+++ src/components/DocumentPage.js
@@ -1,29 +1,43 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-import "./Document.css"
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./Document.css";
 
 const DocumentPage = () => {
-    const {state} = useLocation()
-const {id, name ,data} = state;
+  const { state } = useLocation();
+  const { id, name, data } = state;
 
-    return (
-        <div>
-            <h1>{state.name}</h1>
-            <div className="detail-container">
-                <div className="">
-                    <h3> Label </h3>
-                    <hr />
-                    <p>
-                        <button className='btn-1'>Tittle</button>
-                        <button className='btn-2'> Author</button>
-                    </p>
-                    
-                </div>
-
-            </div>
-            
+  return (
+    <div>
+      <button>
+        <Link to="/">Home Page</Link>
+      </button>
+      <h1>{state.name}</h1>
+      <div className="detail-container">
+        {/* Left side */}
+        <div className="">
+          <h3> Label </h3>
+          <hr />
+          <p>
+            <button className="btn-1">Tittle</button>
+            <button className="btn-2"> Author</button>
+          </p>
+          <p>
+            <h3> Boxes </h3>
+            <hr />
+          </p>
         </div>
-    );
+        {/* Right Side  */}
+        <div className="">
+          <iframe
+            src={data}
+            height="700px"
+            width="100%"
+            name="iframe_a"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DocumentPage;
